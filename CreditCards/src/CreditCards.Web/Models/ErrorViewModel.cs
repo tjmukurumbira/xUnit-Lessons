772bf021302d29f79f7c5a42933cd3e99f0403d0
James Mukurumbira <tjmukurumbira@gmail.com>
2019-07-07 07:28:38 +0200

    setup

---
+++ CreditCards/src/CreditCards.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CreditCards.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
